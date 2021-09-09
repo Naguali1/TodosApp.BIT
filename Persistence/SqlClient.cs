a2c3fae199ce8a16993e39366a73898a0b0df0d7
J <naguali.shaka@gmail.com>
2021-09-09 20:30:35 +0300

    Added sqlClient

---
+++ Persistence/SqlClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+     class SqlClient : ISqlClient
+    {
+        private readonly string _connectionString;
+
+        public SqlClient(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public Task<int> ExecuteAsync(string sql, object param = null)
+        {
+            using var connection = new MySqlConnection(_connectionString);
+
+            return connection.ExecuteAsync(sql, param);
+        }
+
+        public Task<IEnumerable<T>> QueryAsync<T>(string sql, object param = null)
+        {
+            using var connection = new MySqlConnection(_connectionString);
+
+            return connection.QueryAsync<T>(sql, param);
+        }
+    }
+}
